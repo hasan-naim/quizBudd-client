807b87495ea53e53e63695e17d31a9b6a08a125e
hasan-naim <hasannaim6246@gmail.com>
2022-10-10 21:43:11 +0600

    header component is done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import "./App.css";
-import { Navbar } from "flowbite-react";
 import { useEffect } from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   useEffect(() => {
     document.getElementsByTagName("html")[0].classList.add("dark");
-    // document.body.classList.add("dark:bg-slate-800");
+    document.body.classList.add("dark:bg-slate-900");
   }, []);
 
   return (
     <div className="App">
-      <Navbar fluid={true} rounded={true} className="duration-1000">
-        <Navbar.Brand href="https://flowbite.com/">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Flowbite
-          </span>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="duration-1000">
-          <Navbar.Link href="/navbars" active={true}>
-            Home
-          </Navbar.Link>
-          <Navbar.Link href="/navbars">About</Navbar.Link>
-          <Navbar.Link href="/navbars">Services</Navbar.Link>
-          <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-          <Navbar.Link href="/navbars">Contact</Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
+      <Header />
     </div>
   );
 }
