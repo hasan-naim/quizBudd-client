84fc8dd0ee08fe8f1131dca425bba7e8bc2458b5
hasan-naim <hasannaim6246@gmail.com>
2022-10-12 17:15:46 +0600

    blog page is done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -33,6 +33,20 @@ function Blog() {
           use it.
         </p>
       </div>
+      <div className="max-w-[900px] mx-auto mt-16 mb-24">
+        <h1 className="text-3xl mb-4 text-[#69c0ff] font-semibold underline">
+          #3 Explain useRef hook.
+        </h1>
+        <p className="text-xl text-[#b5c8d6] mb-4">
+          When we need to get a reference of a DOM element we use the useRef
+          hook. To get a dom element and to manipulate it we need to get the
+          element first, in react we can get this reference by using useRef
+          hook. useRef returns an object. In the object, there will be a current
+          property where the reference is stored. We can also use it as a
+          variable like a state. But the difference is the component will not
+          render when its value changes.
+        </p>
+      </div>
     </div>
   );
 }
